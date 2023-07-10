59772599a192e76382bb46595e1e9fe0568c2a90
mv-guevara <117041073+mv-guevara@users.noreply.github.com>
2023-07-10 16:46:15 -0300

    Update mainapi.js

--- mainapi.js
+++ mainapi.js
@@ -20,15 +20,4 @@ function fetchCurrencyData() {
     }
       window.onload = fetchCurrencyData;
 
-const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-const apiUrl = 'http://data.fixer.io/api/latest?access_key=ec354f24e6091156d89f3ebd5da1afa1&format=1';
 
-fetch(proxyUrl + apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    // Procesa los datos de la respuesta de la API aquí
-  })
-  .catch(error => {
-    console.error('Error al obtener los datos de la API:', error);
-  });
-      
